2a9402523b703899210789c46bb8c88ccfba545d
James <jmhiggins343@gmail.com>
2020-09-23 23:19:54 -0400

    Login/ logout + Start of Profile Page

---
+++ about.php
@@ -0,0 +1,60 @@
+<?php
+require 'includes/header.php'
+?>
+
+<main>
+<link rel="stylesheet" href="css/about.css">
+
+	<div class="about">
+		<h1>About Us</h1>
+		<button class="button"
+			onclick="alert('I do not know how to display text as an element after clicking this button so this will do for now.')">See
+			More</button>
+		<p>Our project team is lead by Joe Rogan. It's entirely possible that Joe Rogan is the most intellectual being
+			on the planet. He only consumes elk meat and wants to know if you have ever tried DMT. </p>
+	</div>
+	<div class="row">
+		<div class="column">
+			<div class="card">
+				<img src="images/Rogan.jpg">
+				<div class="container">
+					<h2>CEO</h2>
+					<p>Joe Rogan</p>
+					<p>dmt@cs230.com</p>
+				</div>
+			</div>
+		</div>
+		<div class="column">
+			<div class="card">
+				<img src="images/Elon.jpg">
+				<div class="container">
+					<h2>CTO</h2>
+					<p>Elon Musk</p>
+					<p>5head@cs230.com</p>
+				</div>
+			</div>
+		</div>
+		<div class="column">
+			<div class="card">
+				<img src="images/Zucc.png">
+				<div class="container">
+					<h2>CMO</h2>
+					<p>Mark Zuckerberg</p>
+					<p>zucc@cs230.com</p>
+				</div>
+			</div>
+		</div>
+		<div class="column">
+			<div class="card">
+				<img src="images/Bezos.jpg">
+				<div class="container">
+					<h2>Intern</h2>
+					<p>Jeff Bezos</p>
+					<p>lizard@cs230.com</p>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+
